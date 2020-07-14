25b5507e27a1fa9c3a7994a05dddb87d2246b2f3
George Williams <george412williams@gmail.com>
2020-07-14 09:51:52 -0500

    added to gitignore added notes to first servlet added new ex servlet

--- src/main/java/DadJokeServlet.java
+++ src/main/java/DadJokeServlet.java
@@ -2,19 +2,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "GanymedeServlet", urlPatterns = "/ganymede")
-public class GanymedeServlet extends HttpServlet {
+
+@WebServlet(name = "DadJokeServlet", urlPatterns = "/dadjoke")
+public class DadJokeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // now can write a response, need to capture a param so set response content type
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello Ganymede!</h1>");
-        //this is more of a placeholder, these 3 lines, of what can normally go there
-            //intellij will auto complete with a throws servlet exception
-
+        // getting a passed param so
+        String joke = request.getParameter("joke");
     }
+
 }
