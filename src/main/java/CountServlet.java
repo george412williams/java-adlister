042c35e54da7f1ec26059a8cbd5ac45b42417dde
George Williams <george412williams@gmail.com>
2020-07-14 11:50:17 -0500

    working counter bonus works normally not reacting to url

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -8,10 +8,12 @@ import java.io.PrintWriter;
 @WebServlet(name = "CountServlet", urlPatterns = "/count")
 public class CountServlet extends HttpServlet {
 
+    int pageVisit;
+    //String reset;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
 
         //Create a page that displays a number that goes up
         // by one every time the /count page is viewed.
@@ -21,7 +23,16 @@ public class CountServlet extends HttpServlet {
         //Allow the user to pass a parameter in the query
         // string to reset the counter.
 
+        String reset = request.getParameter("reset");
+        if (reset == "yes"){
+            pageVisit = 1;
+        } else {
+        pageVisit++;
+        }
+
+        //pageVisit++;
+
         PrintWriter out = response.getWriter();
-        out.println();
+        out.println("Page Visits: " + pageVisit);
     }
 }
